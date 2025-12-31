de0aac43651f06b6073e684ad1382f669eef4d07
Lawkik333 <khoundloukik97@gmail.com>
2025-12-31 21:46:19 +0530

    router added

--- app.js
+++ app.js
@@ -1,62 +1,13 @@
 import express from "express";
-import fs from "fs/promises";
-import path from "path";
-import crypto from "crypto";
+import {shornerRoutes} from "./routes/shortner.routes.js";
+
 const port=process.env.PORT||3000;
-const filePath=path.join(import.meta.dirname,"views","index.htm");
-const DATA_FILE=path.join(import.meta.dirname,"data","links.json");
-const loadLink= async ()=>{
-    try {
-        const links=await fs.readFile(DATA_FILE,"utf-8");
-        return JSON.parse(links);
-    } catch (error) {
-        if(error.code==="ENOENT")
-        {
-            await fs.writeFile(DATA_FILE,JSON.stringify({}));
-        }
-        return {};
-    } throw error;
-}
-const savelink=async (links)=>{
-    await fs.writeFile(DATA_FILE,JSON.stringify(links));
-}
 
 const app=express();
 app.use(express.urlencoded({extended:true}));
 app.use("/",express.static("public"));
-app.get("/",async (req,res)=>{
-    const links=await loadLink();
-    
-    const file=await fs.readFile(filePath); 
-    const content=file.toString().replaceAll("{{URL_SHORTNER}}",Object.entries(links).map(
-        ([shortCode,url])=>
-        `<li><a href="/${shortCode}" target="_blank">${req.host}/${shortCode}</a></li>`
-    ).join(""));
-    
-    return res.send(content);
-});
-app.post("/",async (req,res)=>{
-    const links=await loadLink();
-    const {url,shortCode}=req.body;
-    const finalShortCode=shortCode||crypto.randomBytes(4).toString("hex");
-    if(links[finalShortCode])
-    {
-         res.redirect("/");
-    }
-    links[finalShortCode]=url;
-    await savelink(links);
-    res.redirect("/")
-});
-app.get("/:shortlink",async (req,res)=>{
-    const {shortlink}=req.params;
-    const links=await loadLink();
-    if(!links[shortlink]) res.redirect("/");
-    else
-    res.redirect(links[shortlink]);
-    
-    
-});
 
+app.use(shornerRoutes);
 
 
 
